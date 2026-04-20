e35e8c8a12db62f09edaacb4e0133c027ff9e37b
Fashad Ahmed <fashad.ahmed20@gmail.com>
2026-04-20 23:25:40 +0200

    feat: Add Read method and support for ARRAY and BULK types in RESP reader

--- internal/resp/resp.go
+++ internal/resp/resp.go
@@ -69,3 +69,64 @@ func (r *Resp) readInteger() (x int, n int, err error) {
 	}
 	return int(i64), n, nil
 }
+
+
+
+/**
+	Read parses the incoming byte stream and returns a structured Value
+*/
+func (r *Resp) Read() (Value, error) {
+	_type, err := r.reader.ReadByte()
+	if err != nil {
+		return Value{}, err
+	}
+
+	switch _type {
+	case ARRAY:
+		return r.readArray()
+	case BULK:
+		return r.readBulk()
+	default:
+		fmt.Printf("Unknown type: %v", string(_type))
+		return Value{}, nil
+	}
+}
+
+func (r *Resp) readArray() (Value, error) {
+	v := Value{}
+	v.Typ = "array"
+
+	len, _, err := r.readInteger()
+	if err != nil {
+		return v, err
+	}
+
+	v.Array = make([]Value, len)
+	for i := 0; i < len; i++ {
+		val, err := r.Read() 
+		if err != nil {
+			return v, err
+		}
+		v.Array[i] = val
+	}
+
+	return v, nil
+}
+
+func (r *Resp) readBulk() (Value, error) {
+	v := Value{}
+	v.Typ = "bulk"
+
+	len, _, err := r.readInteger()
+	if err != nil {
+		return v, err
+	}
+
+	bulk := make([]byte, len)
+	r.reader.Read(bulk)
+	v.Str = string(bulk)
+
+	r.readLine()
+
+	return v, nil
+}
