1b2df496a7161b3b3f40f4f1fead9b56a3d2825f
Fashad Ahmed <fashad.ahmed20@gmail.com>
2026-04-20 23:06:33 +0200

    feat: Add readLine and readInteger methods to RESP reader

--- internal/resp/resp.go
+++ internal/resp/resp.go
@@ -35,3 +35,37 @@ type Resp struct {
 func NewResp(rd io.Reader) *Resp {
 	return &Resp{reader: bufio.NewReader(rd)}
 }
+
+
+
+
+/**
+	readLine reads byte by byte until it hits \r\n
+*/
+func (r *Resp) readLine() (line []byte, n int, err error) {
+	for {
+		b, err := r.reader.ReadByte()
+		if err != nil {
+			return nil, 0, err
+		}
+		n += 1
+		line = append(line, b)
+		
+		if len(line) >= 2 && line[len(line)-2] == '\r' {
+			break
+		}
+	}
+	return line[:len(line)-2], n, nil
+}
+
+func (r *Resp) readInteger() (x int, n int, err error) {
+	line, n, err := r.readLine()
+	if err != nil {
+		return 0, 0, err
+	}
+	i64, err := strconv.ParseInt(string(line), 10, 64)
+	if err != nil {
+		return 0, n, err
+	}
+	return int(i64), n, nil
+}
